850debcc71318d4c76e3824e7728877e3853b1ac
Mike Hernandez <mkhern@gmail.com>
2018-03-02 14:51:37 -0800

    Uses ref to log Map instance on click.

--- leafletSandbox/src/App.jsx
+++ leafletSandbox/src/App.jsx
@@ -8,6 +8,7 @@ const tileSet = `https://api.mapbox.com/styles/v1/mikehern/cje36zhaq2kvw2smq8a8r
 const position = [37.752, -122.4312];
 const zoomLevel = 11.5;
 const mapAttribution = '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>';
+
 const icon = L.icon({
   iconUrl: 'http://www.allwhitebackground.com/images/1/Marker-3.jpg',
 
@@ -19,31 +20,68 @@ const icon = L.icon({
 
 
 export class MapView extends Component {
-  constructor(props){
-    super(props);
+
+  constructor(){
+    super();
     this.state = {
+      hasLocation: false,
+      latlng: {
+        lat: 37.752,
+        lng: -122.4318
+      },
       currentZoomLevel: 11.5
     }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleLocationFound = this.handleLocationFound.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('after component mounted, state is: ', this.state);
+  }
+
+  handleClick() {
+    console.log('clicked! center is: ', this.clickInput.leafletElement.getCenter());
+    console.log('clicked! zoom is: ', this.clickInput.leafletElement.getZoom());
+    console.log('clicked! bounds is: ', this.clickInput.leafletElement.getBounds());
+    console.log('clicked! size is: ', this.clickInput.leafletElement.getSize());
+    console.log('clicked! pixelBounds is: ', this.clickInput.leafletElement.getPixelBounds());
+    console.log('clicked! pixelOrigin is: ', this.clickInput.leafletElement.getPixelOrigin());
+    console.log('clicked! panes is: ', this.clickInput.leafletElement.getPanes());
+    console.log('clicked! container is: ', this.clickInput.leafletElement.getContainer());
+  }
+
+  handleLocationFound(e){
+    this.setState({
+      hasLocation: true,
+      latlng: <e className="latlng"></e>,
+    })
   }
 
   render() {
+    const marker = this.state.hasLocation ? (
+      <Marker position={this.state.latlng} icon={icon}>
+        <Popup>
+          <span>You have arrived.</span>
+        </Popup>
+      </Marker>
+    ) : null;
+
     return(
       <div>
         <Map
-          center={position}
+          center={this.state.latlng}
+          onClick={this.handleClick}
+          onLocationFound={this.handleLocationFound}
+          ref={(input) => {this.clickInput = input;}}
           zoom={this.state.currentZoomLevel}
+          animate={true}
         >
           <TileLayer
             url={tileSet}
             attribution={mapAttribution}
           />
-          <Marker position={position} icon={icon}>
-            <Popup>
-              <span>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </span>
-            </Popup>
-          </Marker>
+          {marker}
         </Map>
       </div>
     )
