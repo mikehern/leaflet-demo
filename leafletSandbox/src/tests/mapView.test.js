297a73db4078bd42eed9f0ad96ebb35ad9387f03
Mike Hernandez <mkhern@gmail.com>
2018-03-12 16:08:47 -0700

    Use enzyme to test for mapView state.

--- leafletSandbox/src/tests/mapView.test.js
+++ leafletSandbox/src/tests/mapView.test.js
@@ -3,28 +3,9 @@ import MapView from '../mapView';
 import { shallow } from 'enzyme';
 
 describe('Initial map loads with markers', () => {
-  xit('Displays a map centered on San Francisco', () => {
+  it('Displays a map centered on San Francisco', () => {
     const wrapper = shallow(<MapView />);
     const mapLat = wrapper.state().lat;
     expect(mapLat).toEqual(37.752);
-  })
-
-  xit('Mapview contains a map', () => {
-    const wrapper = shallow(
-      <MapView>
-        <div className="leaflet-pane" />
-      </MapView>
-    );
-    expect(wrapper.contains(<div className="leaflet-pane" />)).to.equal(true);
-  })
-
-  it('Displays the zoom level above the map', () => {
-    const wrapper = shallow(
-      
-      <MapView>
-        <div>Zoom level is 11.5</div>
-      </MapView>
-    )
-    expect(wrapper.contains(<div>Zoom level is 11.5</div>)).toEqual(true);
   })
 })
